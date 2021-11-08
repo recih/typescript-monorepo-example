760d9f97f01b5705ac03349b5616080cd3dcacf7
recih <recih@users.noreply.github.com>
2021-11-08 15:58:33 +0800

    chore: use `eslint-config-alloy`
    see https://github.com/AlloyTeam/eslint-config-alloy

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    // see https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+    {
+      files: ['*.ts'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
